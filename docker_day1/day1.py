e503bd1338e9efd6236f1d1ac3f83139daad8709
Andy Xiang <andy@ttyn.me>
2018-10-02 12:42:04 -0700

    sort out the path and format

--- docker_day1/day1.py
+++ docker_day1/day1.py
@@ -1,5 +1,17 @@
+# === Day1 Ideas covered ===
 
-# === hour 1 day 1 ===
+"""
+
+1. dev env deployment - ** docker **
+2. dev env - ** docker-compose **
+3. dev env integration - ** docker-compose **
+4. dev tool - for this static site we use ** pycco ** to generate html
+5. delivery  - we publish the html site to ** aws s3 **
+6. version ctrl - ** github **, we might use aws CodeCommit, etc
+
+"""
+
+# === Day1 Section 1 ===
 
 """
 minimal Dockerfile nginx alpine
@@ -20,76 +32,78 @@ src: https://hub.docker.com/_/nginx/
     docker stop day1
   
     docker build
-        -t tag
+                            -t tag      # add tag as image name
     docker run
-        -p mapping container 80 to host 8080
-        -d run detached
+                            -p 8080:80  # mapping container 80 to host 8080
+                            -d          # run detached
+                            --name      # container name
 
 """
 use docker-compose to build and run
 """
 
     docker-compose up --build -d
-        --build if no container
-        --no-recreate if there is container, do not recreate it
+                        --build         # build if no container
+                        --no-recreate   # if there is container, do not recreate it
 
-    open http://localhost:8080
+    open http://localhost:8080          # local test
 
 
-# === hour 2 day 1 ===
+# === Day1 Section 2 ===
 
 """
 docker-compose
+
 if not specify image it will use <project>_<service> as container name
-"""
 
-    docker-compose container name <project>_<service>
-    docker-compose --project-name foo build bar
-    docker images foo_bar
-
-"""
 with already built image, you can remove [build] from docker-compose.yml
 add the specified image and container name
+
     image: my_custom_nginx_image
     container_name: nginx
 
 """
+    docker image image_id tag image_name
+    docker-compose up -d
 
-# === hour 3 day 1 ===
+# === Day1 Section 3 ===
 
 """
 1. ** docker ** - use docker cmd to clean up
 2. ** docker-compose ** - stop and/or cleanup
 """
+
     docker container rm container_name
     docker image rm image_name
-        -f force
-    docker image prune                  # remove unused images
+                            -f      # force
+    docker image prune              # remove unused images
     docker images
 
     docker-compose ps
     docker-compose stop
-        stop    # stop all containers
-        rm -f   # remove all containers
+                            stop    # stop all containers
+                            rm -f   # remove all containers
     docker-compose down --rmi all
-        down    # stop and remove containers networks, volumes and images
+                            down    # stop and remove containers networks, volumes and images
 
     docker-compose up -d
 
 
-# === hour 4 day 1 ===
+# === Day1 Section 4 ===
 
 """
 generate document from pyfile
 """
     pip install pycco
     pycco docker_day1/*.py -d html_output -p -i
-        -p  # preservce path
-        -d  # output directory
-        -i  # generate index
-    python -m http.server 8000      # test
+                            -p  # preservce path
+                            -d  # output directory
+                            -i  # generate index
+    # local test
+    python -m http.server 8000      
 
-# === hour 5 day 1 ===
+
+# === Day1 Section 5 ===
 
 """
 use aws s3 to host the site
@@ -117,16 +131,19 @@ aws s3api put-bucket-website --bucket 2018-test-bucket-andydocket \
 aws s3 sync html_output/ s3://2018-test-bucket-andydocket
 aws s3 sync assets s3://2018-test-bucket-andydocket/assets
 
+
+# === Day1 Section 6 ===
+
 """
-# === hour 6 day 1 ===
 sync with github
+"""
 
 git remote add github git@github.com:arbeitandy/mywaytoday.git
 git pull github master --allow-unrelated-histories
 
 git push github master --all
 
-"""
+
 """
 made by [Pycco][pycco]
 
@@ -135,5 +152,3 @@ made by [Pycco][pycco]
 [pycco]: https://pycco-docs.github.io/pycco/
 [home]: http://d.fogtown.us/
 """
-
-
